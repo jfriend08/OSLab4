82f44fc9002ca82d57c4221b9f1ce96ae744cc79
jfriend08 <ys486@cornell.edu>
2014-07-29 10:04:51 -0400

    --	add and issue the first task

--- isoched.cpp
+++ isoched.cpp
@@ -1,6 +1,7 @@
 /*
 Usage: ./iosched –s<schedalgo> <inputfile>
 --	read in file
+--	add and issue the first task
 
 
 
@@ -27,11 +28,77 @@ Usage: ./iosched –s<schedalgo> <inputfile>
 using namespace std;
 int c, n1, n2;
 string sche, line;
-vector< vector<int> > tasks_v;
+int TIME=1, curTrack=0, issueCount=0, addCount=0;
+
 vector<int> tasks_tmp;
 
-int main(int argc, char *argv[]){
+struct Job{
+	int t;
+	int track;
+};
+vector< Job > tasks_v;
 
+class Scheduler{
+public:
+	virtual int select(vector<Job>* tasks, deque<Job>& Run_dq){return 0;}
+
+};
+
+class FIFO : public Scheduler{
+public:
+	int select(vector<Job>* tasks, deque<Job>& Run_dq){
+		return 0;
+	}
+
+};
+
+class IOsche{
+public:
+	vector < Job >* tasks;
+	deque<Job> Run_dq;
+	Scheduler* scheduler;	
+	int issuedT;
+	
+	void print(){
+		cout<<"tasks"<<endl;
+		for(int i=0; i<tasks->size(); i++){cout<<tasks->at(i).t<<" "<<tasks->at(i).track<<endl;}
+		cout<<"run"<<endl;
+		for(int i=0; i<Run_dq.size(); i++){cout<<Run_dq[i].t<<" "<<Run_dq[i].track<<endl;}
+		cout<<"TIME:"<<TIME<<" curTrack:"<<curTrack<<endl;
+		// cout<<scheduler->select(tasks, Run_dq)<<endl;
+	}
+	void add(vector<Job>* tasks, deque<Job> &Run_dq){
+		printf("%d: %5d add %3d\n",tasks->front().t,addCount, tasks->front().track); addCount++;
+		Run_dq.push_back(tasks->front()); tasks->erase(tasks->begin());
+	}
+	
+	void issue(vector<Job>* tasks, deque<Job> &Run_dq){
+		int run_idx=scheduler->select(tasks, Run_dq);
+		printf("%d: %5d issue %3d %d\n",TIME, run_idx,Run_dq[run_idx].track, curTrack);	
+		TIME=TIME+Run_dq[run_idx].track; curTrack=Run_dq[run_idx].track;
+
+	}
+
+	void change(){
+		while(tasks->size()!=0 | Run_dq.size()!=0){
+			if(Run_dq.size()==0){
+				add(tasks, Run_dq);
+			}
+			else if(tasks->at(0).t<Run_dq.front().t){
+				add(tasks, Run_dq);	
+			}
+			issue(tasks, Run_dq);
+			print();
+		}
+		
+	}
+	
+
+
+};
+
+int main(int argc, char *argv[]){
+	IOsche IO;
 	while((c=getopt(argc,argv,"s:")) !=-1){
 		switch (c){
 			case 's':
@@ -49,14 +116,31 @@ int main(int argc, char *argv[]){
 	else{
 		while(!fin.eof()){
 			getline(fin, line);
-			if (line[0]!='#' && line !=""){				
-				std::stringstream(line)>>n1>>n2;tasks_tmp.push_back(n1);tasks_tmp.push_back(n2);
-        		tasks_v.push_back(tasks_tmp);tasks_tmp.clear();
-			}					
+			if (line[0]!='#' && line !=""){		
+				Job job_tmp;
+				std::stringstream(line)>>n1>>n2;job_tmp.t=n1; job_tmp.track=n2;tasks_v.push_back(job_tmp);        		}					
 		}		
 	}
 	fin.close();
+	if (sche=="i"){IO.scheduler = new FIFO;}
+	
+	IO.tasks = &tasks_v;
+	IO.change();
 
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
