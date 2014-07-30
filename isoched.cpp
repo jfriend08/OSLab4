2afe35a4afb14a492b96509045a7ec6f66a4fdc1
jfriend08 <ys486@cornell.edu>
2014-07-30 00:47:14 -0400

    --	now all process and SUM are correct for method i

--- isoched.cpp
+++ isoched.cpp
@@ -3,6 +3,7 @@ Usage: ./iosched –s<schedalgo> <inputfile>
 --	read in file
 --	add and issue the first task
 --	now all processes are correct for method i
+--	now all process and SUM are correct for method i
 
 */
 
@@ -25,9 +26,11 @@ Usage: ./iosched –s<schedalgo> <inputfile>
 
 
 using namespace std;
-int c, n1, n2, D_flag=0;
+int c, n1, n2, D_flag=0, o_flag=0;
 string sche, line;
 int TIME=1, curTrack=0, issueCount=0, addCount=0, IsueTime=0;
+int total_time=0, tot_movement=0, max_waittime=0;
+double avg_turnaround=0, avg_waittime=0;
 
 vector<int> tasks_tmp;
 
@@ -36,6 +39,13 @@ struct Job{
 	int track;
 };
 vector< Job > tasks_v;
+
+struct Record{
+	int arrive_t;
+	int diskStart_t;
+	int diskEnd_t;
+};
+vector< Record > Record_v;
 
 class Scheduler{
 public:
@@ -70,24 +80,45 @@ public:
 	}
 	void add(vector<Job>* tasks, deque<Job> &Run_dq){
 		if(Run_dq.empty())TIME=tasks->front().t;
-		printf("%d: %5d add %d\n",tasks->front().t, addCount, tasks->front().track); addCount++;
+		if(o_flag==1)printf("%d: %5d add %d\n",tasks->front().t, addCount, tasks->front().track); addCount++;
+		Record record_tmp; record_tmp.arrive_t=tasks->front().t; Record_v.push_back(record_tmp);
 		Run_dq.push_back(tasks->front()); tasks->erase(tasks->begin());
 		if(D_flag==1)print();
 	}
 	
 	void issue(vector<Job>* tasks, deque<Job> &Run_dq){
 		run_idx=scheduler->select(tasks, Run_dq);
-		printf("%d: %5d issue %d %d\n",TIME, issueCount, Run_dq[run_idx].track, curTrack); issueCount++;
+		if(o_flag==1)printf("%d: %5d issue %d %d\n",TIME, issueCount, Run_dq[run_idx].track, curTrack); issueCount++;
+		Record_v[issueCount-1].diskStart_t=TIME;
+		if(curTrack!=Run_dq[run_idx].track)tot_movement=tot_movement + abs(curTrack - Run_dq[run_idx].track);
 		TIME=TIME+abs(curTrack-Run_dq[run_idx].track); curTrack=Run_dq[run_idx].track;
 		currentJob=Run_dq[run_idx];
 		if(D_flag==1)print();
 
 	}
 	void finish(vector<Job>* tasks, deque<Job> &Run_dq){
-		printf("%d: %5d finish %d\n",TIME, issueCount-1, TIME-Run_dq[run_idx].t);	
+		if(o_flag==1)printf("%d: %5d finish %d\n",TIME, issueCount-1, TIME-Run_dq[run_idx].t);	
+		Record_v[issueCount-1].diskEnd_t=TIME;
 		Finish_dq.push_back(Run_dq[run_idx]);
 		Run_dq.erase(Run_dq.begin()+run_idx);
 		if(D_flag==1)print();
+	}
+	void SUM(){
+		if(o_flag==1) cout<<"IOREQS INFO"<<endl;
+		total_time=Record_v[Record_v.size()-1].diskEnd_t; 
+		// tot_movement=total_time-1;
+		for (int i=0; i<Record_v.size(); i++){			
+			avg_turnaround = avg_turnaround + (Record_v[i].diskEnd_t - Record_v[i].arrive_t);
+			double waittime=Record_v[i].diskStart_t - Record_v[i].arrive_t;
+			avg_waittime = avg_waittime + waittime;
+			if(waittime>max_waittime)max_waittime=waittime;
+			if(o_flag==1) printf("%5d: %5d %5d %5d\n",i, Record_v[i].arrive_t, Record_v[i].diskStart_t, Record_v[i].diskEnd_t);
+		}
+		avg_turnaround=avg_turnaround/Record_v.size();
+		avg_waittime=avg_waittime/Record_v.size();
+
+		printf("SUM: %d %d %.2f %.2f %d\n",total_time, tot_movement, avg_turnaround, avg_waittime, max_waittime);
+
 	}
 
 	void change(){
@@ -99,25 +130,10 @@ public:
 				finish(tasks, Run_dq);			
 			}
 			if(!Run_dq.empty()) issue(tasks, Run_dq);
-			else if (Run_dq.empty()&& !tasks->empty()){add(tasks, Run_dq);issue(tasks, Run_dq);}
-			// print();
+			else if (Run_dq.empty()&& !tasks->empty()){add(tasks, Run_dq);issue(tasks, Run_dq);}			
 			
-
 		}
-		
-
-
-		// while(tasks->size()!=0 | Run_dq.size()!=0){
-		// 	if(Run_dq.size()==0){
-		// 		add(tasks, Run_dq);
-		// 	}
-		// 	else if(tasks->at(0).t<TIME){
-		// 		add(tasks, Run_dq);	
-		// 	}
-		// 	issue(tasks, Run_dq);
-		// 	print();
-		// }
-		
+		SUM();		
 	}
 	
 
@@ -126,7 +142,7 @@ public:
 
 int main(int argc, char *argv[]){
 	IOsche IO;
-	while((c=getopt(argc,argv,"s:D")) !=-1){
+	while((c=getopt(argc,argv,"s:D:o")) !=-1){
 		switch (c){
 			case 's':
 				sche.assign(optarg);
@@ -134,11 +150,14 @@ int main(int argc, char *argv[]){
 			case 'D':
 				D_flag=1;
 				break;
+			case 'o':
+				o_flag=1;
+				break;
 			default:
 				abort();
 		}
 	}
-	cout <<"sche:"<<sche<<endl;
+	// cout <<"sche:"<<sche<<endl;
 	if(sche==""){cout<<"please specify schedule method"<<endl; return 0;}
 
 	ifstream fin (argv[argc-1]);
